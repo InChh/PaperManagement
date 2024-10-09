cf9b581c4482715954524a40eb61ec0b8cb8ae1d
InChh <2319397152@qq.com>
2024-10-09 22:18:26 +0800

    feat: Add Database Migration

---
+++ src/Wf.PaperManagement.EntityFrameworkCore/Migrations/20241009141525_UpdateWorkerAddIsOnDuty.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wf.PaperManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWorkerAddIsOnDuty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnDuty",
+                table: "PmWorkers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityId",
+                table: "AbpEntityChanges",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOnDuty",
+                table: "PmWorkers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityId",
+                table: "AbpEntityChanges",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128,
+                oldNullable: true);
+        }
+    }
+}
