ff1237a1549c90ce23cc5db80587d86dbe846fad
InChh <2319397152@qq.com>
2024-10-11 17:11:05 +0800

    fix: 关闭worker表的软删除功能，防止队员工号键重复无法添加新队员

--- src/Wf.PaperManagement.Application/Workers/WorkerAppService.cs
+++ src/Wf.PaperManagement.Application/Workers/WorkerAppService.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Volo.Abp;
 using Volo.Abp.Application.Dtos;
+using Volo.Abp.Data;
 
 namespace Wf.PaperManagement.Workers;
 
@@ -13,11 +15,15 @@ public class WorkerAppService : PaperManagementAppService, IWorkerAppService
 {
     private readonly WorkerManager _workerManager;
     private readonly IWorkerRepository _workerRepository;
+    private readonly IDataFilter<ISoftDelete> _softDeleteFilter;
 
-    public WorkerAppService(WorkerManager workerManager, IWorkerRepository workerRepository)
+
+    public WorkerAppService(WorkerManager workerManager, IWorkerRepository workerRepository,
+        IDataFilter<ISoftDelete> softDeleteFilter)
     {
         _workerManager = workerManager;
         _workerRepository = workerRepository;
+        _softDeleteFilter = softDeleteFilter;
     }
 
     public async Task<WorkerDto> GetByWorkerIdAsync(int workerId)
@@ -53,7 +59,7 @@ public class WorkerAppService : PaperManagementAppService, IWorkerAppService
     [Authorize(Roles = "leader")]
     public async Task<WorkerDto> CreateAsync(CreateWorkerDto input)
     {
-        var worker = await _workerManager.CreateAsync(input.UserId, input.WorkerId,input.Name);
+        var worker = await _workerManager.CreateAsync(input.UserId, input.WorkerId, input.Name);
         await _workerRepository.InsertAsync(worker);
         return ObjectMapper.Map<Worker, WorkerDto>(worker);
     }
@@ -62,7 +68,7 @@ public class WorkerAppService : PaperManagementAppService, IWorkerAppService
     public async Task<WorkerDto> UpdateAsync(Guid userId, UpdateWorkerDto input)
     {
         var worker = await _workerRepository.GetByUserId(userId);
-        
+
         if (input.WorkerId is not null)
         {
             await _workerManager.SetWorkerId(worker, input.WorkerId.Value);
@@ -80,19 +86,25 @@ public class WorkerAppService : PaperManagementAppService, IWorkerAppService
     [Authorize(Roles = "leader")]
     public async Task<WorkerDto> DeleteByWorkerIdAsync(int workerId)
     {
-        var worker = await _workerRepository.GetByWorkerId(workerId);
-        await _workerManager.OffDuty(worker);
-        await _workerRepository.DeleteAsync(worker);
-        return ObjectMapper.Map<Worker, WorkerDto>(worker);
+        using (_softDeleteFilter.Disable())
+        {
+            var worker = await _workerRepository.GetByWorkerId(workerId);
+            await _workerManager.OffDuty(worker);
+            await _workerRepository.DeleteAsync(worker);
+            return ObjectMapper.Map<Worker, WorkerDto>(worker);
+        }
     }
 
     [Authorize(Roles = "leader")]
     public async Task<WorkerDto> DeleteByUserIdAsync(Guid userId)
     {
-        var worker = await _workerRepository.GetByUserId(userId);
-        await _workerManager.OffDuty(worker);
-        await _workerRepository.DeleteAsync(worker);
-        return ObjectMapper.Map<Worker, WorkerDto>(worker);
+        using (_softDeleteFilter.Disable())
+        {
+            var worker = await _workerRepository.GetByUserId(userId);
+            await _workerManager.OffDuty(worker);
+            await _workerRepository.DeleteAsync(worker);
+            return ObjectMapper.Map<Worker, WorkerDto>(worker);
+        }
     }
 
     [Authorize(Roles = "leader")]
